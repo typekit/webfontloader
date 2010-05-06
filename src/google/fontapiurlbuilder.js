5fa2527cc736ba73da78ee4d57aa486e347f468a
Ryan Carver <ryan@typekit.com>
2010-05-06 11:47:14 -0700

    source for the google module

---
+++ src/google/fontapiurlbuilder.js
@@ -0,0 +1,26 @@
+webfont.FontApiUrlBuilder = function(apiUrl) {
+  this.apiUrl_ = apiUrl || webfont.FontApiUrlBuilder.DEFAULT_API_URL;
+  this.fontFamilies_ = null;
+};
+
+webfont.FontApiUrlBuilder.DEFAULT_API_URL = 'http://themes.googleusercontent.com/fonts/api';
+
+webfont.FontApiUrlBuilder.prototype.setFontFamilies = function(fontFamilies) {
+  // maybe clone?
+  this.fontFamilies_ = fontFamilies;
+};
+
+webfont.FontApiUrlBuilder.prototype.build = function() {
+  if (!this.fontFamilies_) {
+    throw new Error('No fonts to load !');
+  }
+  var length = this.fontFamilies_.length;
+  var sb = [];
+
+  for (var i = 0; i < length; i++) {
+    sb.push(encodeURIComponent(this.fontFamilies_[i]));
+  }
+  var url = this.apiUrl_ + '?family=' + sb.join('%7C'); // '|' escaped.
+
+  return url;
+};
---
+++ src/google/googlefontapi.js
@@ -0,0 +1,44 @@
+webfont.GoogleFontApi = function(userAgent, domHelper, configuration) {
+  this.userAgent_ = userAgent;
+  this.domHelper_ = domHelper;
+  this.configuration_ = configuration;
+};
+
+webfont.GoogleFontApi.NAME = 'google';
+
+webfont.GoogleFontApi.prototype.supportUserAgent = function(userAgent, support) {
+  if (userAgent.getPlatform().match(/iPad|iPod|iPhone/) != null) {
+    support(false);
+  }
+  return support(userAgent.isSupportingWebFont());
+};
+
+webfont.GoogleFontApi.prototype.load = function(onReady) {
+  var fontApiUrlBuilder = new webfont.FontApiUrlBuilder(
+      this.configuration_['api']);
+  var fontFamilies = this.configuration_['family'];
+  var domHelper = this.domHelper_;
+  var nonBlockingIe = this.userAgent_.getName() == 'MSIE' &&
+      this.configuration_['blocking'] != true;
+
+  fontApiUrlBuilder.setFontFamilies(fontFamilies);
+
+  if (nonBlockingIe) {
+    domHelper.whenExists('body', function() {
+      domHelper.insertInto('head', domHelper.createCssLink(
+          fontApiUrlBuilder.build()));
+    });
+  } else {
+    domHelper.insertInto('head', domHelper.createCssLink(
+        fontApiUrlBuilder.build()));
+  }
+
+  onReady(fontFamilies);
+};
+
+WebFont.addModule(webfont.GoogleFontApi.NAME, function(configuration) {
+  var userAgentParser = new webfont.UserAgentParser(navigator.userAgent);
+  var userAgent = userAgentParser.parse();
+  return new webfont.GoogleFontApi(userAgent,
+      new webfont.DomHelper(document), configuration);
+});
