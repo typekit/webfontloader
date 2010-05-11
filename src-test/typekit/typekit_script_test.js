e776719dae6804f8a6ca217b05eb65524e2bad1d
Ryan Carver <ryan@typekit.com>
2010-05-11 11:59:37 -0700

    let Typekit determine user agent support

--- src-test/typekit/typekit_script_test.js
+++ src-test/typekit/typekit_script_test.js
@@ -1,51 +1,6 @@
 var TypekitScriptTest = TestCase('TypeKitScriptTest');
 
-TypekitScriptTest.prototype.testInsertScriptTag = function() {
-  var configuration = {
-    'id': 'abc'
-  };
-  var insert = '';
-  var src = '';
-  var fakeDomHelper = {
-      insertInto: function(tag, e) {
-        insert = tag;
-      },
-      createScriptSrc: function(srcLink) {
-        src = srcLink;
-      }
-  };
-  var typeKit = new webfont.TypekitScript({}, fakeDomHelper, configuration);
-  var fonts = null;
-
-  typeKit.load(function(fontFamilies) { fonts = fontFamilies; });
-  assertEquals('head', insert);
-  assertEquals('http://use.typekit.com/abc.js', src);
-};
-
-TypekitScriptTest.prototype.testInsertScriptTagWithAlternativeApi = function() {
-  var configuration = {
-    'id': 'abc',
-    'api': '/test'
-  };
-  var insert = '';
-  var src = '';
-  var fakeDomHelper = {
-      insertInto: function(tag, e) {
-        insert = tag;
-      },
-      createScriptSrc: function(srcLink) {
-        src = srcLink;
-      }
-  };
-  var typeKit = new webfont.TypekitScript({}, fakeDomHelper, configuration);
-  var fonts = null;
-
-  typeKit.load(function(fontFamilies) { fonts = fontFamilies; });
-  assertEquals('head', insert);
-  assertEquals('/test/abc.js', src);
-};
-
-TypekitScriptTest.prototype.testCallbackExecution = function() {
+TypekitScriptTest.prototype.testSupportAndLoadLifecycle = function() {
   var configuration = {
     'id': 'abc'
   };
@@ -61,14 +16,55 @@ TypekitScriptTest.prototype.testCallbackExecution = function() {
   };
   var global = {};
   var typeKit = new webfont.TypekitScript(global, fakeDomHelper, configuration);
+
+  // supportUserAgent
+  var userAgent = 'user agent';
+  var isSupport = null;
+
+  typeKit.supportUserAgent(userAgent, function(support) { isSupport = support; });
+  assertEquals('head', insert);
+  assertEquals('http://use.typekit.com/abc.js', src);
+  assertEquals(null, isSupport);
+
+  assertNotNull(global.__typekitScriptModules__);
+  assertNotNull(global.__typekitScriptModules__['abc']);
+  assertNotNull(global.__typekitScriptModules__['abc']['init']);
+  assertEquals('user agent', global.__typekitScriptModules__['abc']['ua']);
+
+  global.__typekitScriptModules__['abc']['init'](true, ['Font1', 'Font2']);
+  assertEquals(true, isSupport);
+
+  // load
   var fonts = null;
 
   typeKit.load(function(fontFamilies) { fonts = fontFamilies; });
-  global.__typekitScriptModules__['abc']([ 'Font1', 'Font2' ]);
-  assertEquals('head', insert);
-  assertEquals('http://use.typekit.com/abc.js', src);
+
   assertNotNull(fonts);
   assertEquals(2, fonts.length);
   assertEquals('Font1', fonts[0]);
   assertEquals('Font2', fonts[1]);
+};
+
+TypekitScriptTest.prototype.testAlternateApi = function() {
+  var configuration = {
+    'id': 'abc',
+    'api': '/test'
+  };
+  var insert = '';
+  var src = '';
+  var fakeDomHelper = {
+      insertInto: function(tag, e) {
+        insert = tag;
+      },
+      createScriptSrc: function(srcLink) {
+        src = srcLink;
+      }
+  };
+  var typeKit = new webfont.TypekitScript({}, fakeDomHelper, configuration);
+  var userAgent = 'user agent';
+  var isSupport = null;
+
+  typeKit.supportUserAgent(userAgent, function(support) { isSupport = support; });
+  assertEquals('head', insert);
+  assertEquals('/test/abc.js', src);
 };
--- src/typekit/typekit_script.js
+++ src/typekit/typekit_script.js
@@ -2,35 +2,48 @@ webfont.TypekitScript = function(global, domHelper, configuration) {
   this.global_ = global;
   this.domHelper_ = domHelper;
   this.configuration_ = configuration;
+  this.fontFamilies_ = [];
 };
 
 webfont.TypekitScript.NAME = 'typekit';
 
 webfont.TypekitScript.prototype.getScriptSrc = function(kitId) {
-  var api = this.configuration_['api'] || 'http://use.typekit.com'
+  var api = this.configuration_['api'] || 'http://use.typekit.com';
   return api + '/' + kitId + '.js';
 };
 
 webfont.TypekitScript.prototype.supportUserAgent = function(userAgent, support) {
-  if (userAgent.getName() == "Opera") {
-      support(false);
-  }
-  return support(userAgent.isSupportingWebFont());
-};
-
-webfont.TypekitScript.prototype.load = function(onReady) {
   var kitId = this.configuration_['id'];
+  var self = this;
 
   if (kitId) {
-    this.domHelper_.insertInto('head', this.domHelper_.createScriptSrc(
-        this.getScriptSrc(kitId)));
-
-    // Set up global listener so that Typekit can tell us the fonts it will provide.
+    // Provide WebFont JS data to Typekit for processing.
     if (!this.global_.__typekitScriptModules__) {
       this.global_.__typekitScriptModules__ = {};
     }
-    this.global_.__typekitScriptModules__[kitId] = onReady;
+
+    // Typekit will call 'init' to indicate whether it supports fonts
+    // and what fonts will be provided.
+    this.global_.__typekitScriptModules__[kitId] = {
+      'ua': userAgent,
+      'init': function(typekitSupports, fontFamilies) {
+        self.fontFamilies_ = fontFamilies;
+        support(typekitSupports);
+      }
+    };
+
+    // Load the Typekit script.
+    var script = this.domHelper_.createScriptSrc(this.getScriptSrc(kitId))
+    this.domHelper_.insertInto('head', script);
+
+  } else {
+    // NOTE: is this right?
+    support(true);
   }
+};
+
+webfont.TypekitScript.prototype.load = function(onReady) {
+  onReady(this.fontFamilies_);
 };
 
 WebFont.addModule(webfont.TypekitScript.NAME, function(configuration) {
