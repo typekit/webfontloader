62d43f8e48a6d6f32111eddec35d4d5e549d1360
Ryan Carver <ryan@typekit.com>
2010-06-14 09:59:01 -0700

    update VERSION constant to the current version

--- lib/webfontloader.rb
+++ lib/webfontloader.rb
@@ -3,7 +3,7 @@ require 'yaml'
 require 'webfontloader/modules'
 
 module WebFontLoader
-  VERSION = '0.0.0'
+  VERSION = '1.0.3'
 
   ProjectRoot = File.expand_path(File.dirname(__FILE__) + "/..")
 
