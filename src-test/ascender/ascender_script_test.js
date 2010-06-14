cc72ef6b21a62c735174653073c5a1fb6cc13ae9
Ryan Carver <ryan@typekit.com>
2010-06-14 09:52:31 -0700

    whitespace

--- src-test/ascender/ascender_script_test.js
+++ src-test/ascender/ascender_script_test.js
@@ -5,7 +5,7 @@ AscenderScriptTest.prototype.testLoadAndFamilyVariations = function(){
   var insert, css, element;
   var key = 'ec2de397-11ae-4c10-937f-bf94283a70c1';
   var onReadyTriggered = false;
-  
+
   var fakeDomHelper = {
       insertInto: function(tag, el) {
         insert = tag;
@@ -21,21 +21,21 @@ AscenderScriptTest.prototype.testLoadAndFamilyVariations = function(){
     key:key,
     families: ['AndyBold','Arial:bold,regular']
   };
-  
+
   var fakeOnReady = function(){
     onReadyTriggered = true;
   };
 
   var as = new webfont.AscenderScript(fakeDomHelper, configuration);
-  
+
   assertFalse(onReadyTriggered);
   as.load(fakeOnReady);
-  
+
   assertEquals('http://webfonts.fontslive.com/css/' + key + '.css', css);
   assertEquals('<link href="http://webfonts.fontslive.com/css/' + key + '.css" type="text/css" />', element);
   assertEquals('head', insert);
   assertTrue(onReadyTriggered);
-  
+
   assertEquals(['n4'], as.parseVariations('regular'));
   assertEquals(['n7'], as.parseVariations('bold'));
   assertEquals(['i4'], as.parseVariations('italic'));
@@ -44,5 +44,5 @@ AscenderScriptTest.prototype.testLoadAndFamilyVariations = function(){
   assertEquals(['n4','n7'], as.parseVariations('regular,bold'));
   assertEquals(['n4','n7'], as.parseVariations('regular,,bold'));
   assertEquals(['n4','n7'], as.parseVariations('n4,n7'));
-  
+
 };
