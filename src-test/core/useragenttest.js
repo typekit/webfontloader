57035287e8ad87b430b349d8cfa2fe88cf1ee8bc
Ryan Carver <ryan@typekit.com>
2010-07-09 10:34:07 -0700

    don't keep ua strings to 80 chars because it makes maintenance hard

--- src-test/core/useragenttest.js
+++ src-test/core/useragenttest.js
@@ -2,8 +2,7 @@ var UserAgentTest = TestCase('UserAgentTest');
 
 UserAgentTest.prototype.testBrowserIsFirefox = function() {
   var userAgentParser = new webfont.UserAgentParser(
-      "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.5; en-US; rv:1.9.2.3) " +
-      "Gecko/20100401 Firefox/3.6.3 GTB7.1");
+      "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.5; en-US; rv:1.9.2.3) Gecko/20100401 Firefox/3.6.3 GTB7.1");
   var userAgent = userAgentParser.parse();
 
   assertEquals("Firefox", userAgent.getName());
@@ -16,8 +15,7 @@ UserAgentTest.prototype.testBrowserIsFirefox = function() {
 
 UserAgentTest.prototype.testBrowserIsChrome = function() {
   var userAgentParser = new webfont.UserAgentParser(
-      "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_5_8; en-US) " +
-      "AppleWebKit/533.2 (KHTML, like Gecko) Chrome/5.0.342.9 Safari/533.2");
+      "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_5_8; en-US) AppleWebKit/533.2 (KHTML, like Gecko) Chrome/5.0.342.9 Safari/533.2");
   var userAgent = userAgentParser.parse();
 
   assertEquals("Chrome", userAgent.getName());
@@ -30,9 +28,7 @@ UserAgentTest.prototype.testBrowserIsChrome = function() {
 
 UserAgentTest.prototype.testBrowserIsSafari = function() {
   var userAgentParser = new webfont.UserAgentParser(
-      "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_5_8; en-us) " +
-      "AppleWebKit/531.21.8 (KHTML, like Gecko) Version/4.0.4 " +
-      "Safari/531.21.10");
+      "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_5_8; en-us) AppleWebKit/531.21.8 (KHTML, like Gecko) Version/4.0.4 Safari/531.21.10");
   var userAgent = userAgentParser.parse();
 
   assertEquals("Safari", userAgent.getName());
@@ -45,8 +41,7 @@ UserAgentTest.prototype.testBrowserIsSafari = function() {
 
 UserAgentTest.prototype.testBrowserIsIE = function() {
   var userAgentParser = new webfont.UserAgentParser(
-      "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; InfoPath.2; " +
-      ".NET CLR 2.0.50727; .NET CLR 3.0.4506.2152; .NET CLR 3.5.30729)");
+      "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; InfoPath.2; .NET CLR 2.0.50727; .NET CLR 3.0.4506.2152; .NET CLR 3.5.30729)");
   var userAgent = userAgentParser.parse();
 
   assertEquals("MSIE", userAgent.getName());
@@ -59,9 +54,7 @@ UserAgentTest.prototype.testBrowserIsIE = function() {
 
 UserAgentTest.prototype.testBrowserIsIPhone = function() {
   var userAgentParser = new webfont.UserAgentParser(
-      "Mozilla/5.0 (iPhone; U; CPU iPhone OS 3_1_2 like Mac OS X; en-us) " +
-      "AppleWebKit/528.18 (KHTML, like Gecko) Version/4.0 Mobile/7D11 " +
-      "Safari/528.16");
+      "Mozilla/5.0 (iPhone; U; CPU iPhone OS 3_1_2 like Mac OS X; en-us) AppleWebKit/528.18 (KHTML, like Gecko) Version/4.0 Mobile/7D11 Safari/528.16");
   var userAgent = userAgentParser.parse();
 
   assertEquals("Safari", userAgent.getName());
@@ -75,8 +68,7 @@ UserAgentTest.prototype.testBrowserIsIPhone = function() {
 
 UserAgentTest.prototype.testBrowserIsAndroid = function() {
   var userAgentParser = new webfont.UserAgentParser(
-      "Mozilla/5.0 (Linux; U; Android 2.1-update1; en-us; Nexus One Build/ERE27) " +
-      "AppleWebKit/530.17 (KHTML, like Gecko) Version/4.0 Mobile Safari/530.17");
+      "Mozilla/5.0 (Linux; U; Android 2.1-update1; en-us; Nexus One Build/ERE27) AppleWebKit/530.17 (KHTML, like Gecko) Version/4.0 Mobile Safari/530.17");
   var userAgent = userAgentParser.parse();
 
   assertEquals("Safari", userAgent.getName());
@@ -88,9 +80,8 @@ UserAgentTest.prototype.testBrowserIsAndroid = function() {
 };
 
 UserAgentTest.prototype.testBrowserIsFirefoxLettersVersion = function() {
-  var userAgentParser = new webfont.UserAgentParser("Mozilla/5.0 (X11; U; " +
-      "Linux i686; ru-RU; rv:1.9.2a1pre) Gecko/20090405 Ubuntu/9.04 (jaunty) " +
-      "Firefox/3.6a1pre");
+  var userAgentParser = new webfont.UserAgentParser(
+      "Mozilla/5.0 (X11; U; Linux i686; ru-RU; rv:1.9.2a1pre) Gecko/20090405 Ubuntu/9.04 (jaunty) Firefox/3.6a1pre");
   var userAgent = userAgentParser.parse();
 
   assertEquals("Firefox", userAgent.getName());
@@ -103,8 +94,7 @@ UserAgentTest.prototype.testBrowserIsFirefoxLettersVersion = function() {
 
 UserAgentTest.prototype.testBrowserIsFirefoxNoVersion = function() {
   var userAgentParser = new webfont.UserAgentParser(
-      "Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.8.1.19) " +
-      "Gecko/20081202 Firefox (Debian-2.0.0.19-0etch1)");
+      "Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.8.1.19) Gecko/20081202 Firefox (Debian-2.0.0.19-0etch1)");
   var userAgent = userAgentParser.parse();
 
   assertEquals("Firefox", userAgent.getName());
@@ -117,9 +107,7 @@ UserAgentTest.prototype.testBrowserIsFirefoxNoVersion = function() {
 
 UserAgentTest.prototype.testBrowserIsIELetterVersion = function() {
   var userAgentParser = new webfont.UserAgentParser(
-      "Mozilla/4.0 (compatible; MSIE 7.0b; Windows NT 5.1; Media Center " +
-      "PC 3.0; .NET CLR 1.0.3705; .NET CLR 1.1.4322; .NET CLR 2.0.50727; " +
-      "InfoPath.1)");
+      "Mozilla/4.0 (compatible; MSIE 7.0b; Windows NT 5.1; Media Center PC 3.0; .NET CLR 1.0.3705; .NET CLR 1.1.4322; .NET CLR 2.0.50727; InfoPath.1)");
   var userAgent = userAgentParser.parse();
 
   assertEquals("MSIE", userAgent.getName());
@@ -131,8 +119,8 @@ UserAgentTest.prototype.testBrowserIsIELetterVersion = function() {
 };
 
 UserAgentTest.prototype.testBrowserIsOpera = function() {
-  var userAgentParser = new webfont.UserAgentParser("Opera/9.80 (Linux i686; " +
-      "U; en) Presto/2.5.22 Version/10.51");
+  var userAgentParser = new webfont.UserAgentParser(
+      "Opera/9.80 (Linux i686; U; en) Presto/2.5.22 Version/10.51");
   var userAgent = userAgentParser.parse();
 
   assertEquals("Opera", userAgent.getName());
@@ -157,8 +145,8 @@ UserAgentTest.prototype.testBrowserIsOperaFirefoxInUAString = function() {
 };
 
 UserAgentTest.prototype.testBrowserIsOperaBeforeVersion10 = function() {
-  var userAgentParser = new webfont.UserAgentParser("Opera/9.64 (X11; Linux " +
-      "i686; U; Linux Mint; nb) Presto/2.1.1");
+  var userAgentParser = new webfont.UserAgentParser(
+      "Opera/9.64 (X11; Linux i686; U; Linux Mint; nb) Presto/2.1.1");
   var userAgent = userAgentParser.parse();
 
   assertEquals("Opera", userAgent.getName());
@@ -184,9 +172,7 @@ UserAgentTest.prototype.testBrowserIsIEOnMac = function() {
 
 UserAgentTest.prototype.testBrowserIsIPad = function() {
   var userAgentParser = new webfont.UserAgentParser(
-      "Mozilla/5.0(iPad; U; CPU iPhone OS 3_2 like Mac OS X; en-us) " +
-      "AppleWebKit/531.21.10 (KHTML, like Gecko) Version/4.0.4 Mobile/7B314 " +
-      "Safari/531.21.10");
+      "Mozilla/5.0(iPad; U; CPU iPhone OS 3_2 like Mac OS X; en-us) AppleWebKit/531.21.10 (KHTML, like Gecko) Version/4.0.4 Mobile/7B314 Safari/531.21.10");
   var userAgent = userAgentParser.parse();
 
   assertEquals("Safari", userAgent.getName());
@@ -199,8 +185,7 @@ UserAgentTest.prototype.testBrowserIsIPad = function() {
 
 UserAgentTest.prototype.testBrowserIsIPod = function() {
   var userAgentParser = new webfont.UserAgentParser(
-      "Mozilla/5.0 (iPod; U; CPU iPhone OS 2_2_1 like Mac OS X; en-us) " +
-      "AppleWebKit/525.18.1 (KHTML, like Gecko) Mobile/5H11a");
+      "Mozilla/5.0 (iPod; U; CPU iPhone OS 2_2_1 like Mac OS X; en-us) AppleWebKit/525.18.1 (KHTML, like Gecko) Mobile/5H11a");
   var userAgent = userAgentParser.parse();
 
   assertEquals("Unknown", userAgent.getName());
@@ -213,8 +198,7 @@ UserAgentTest.prototype.testBrowserIsIPod = function() {
 
 UserAgentTest.prototype.testBrowserIsSafariWithPlusVersion = function() {
   var userAgentParser = new webfont.UserAgentParser(
-      "Mozilla/5.0 (Macintosh; U; PPC Mac OS X 10_4_11; tr) " +
-      "AppleWebKit/528.4+ (KHTML, like Gecko) Version/4.0dp1 Safari/526.11.2");
+      "Mozilla/5.0 (Macintosh; U; PPC Mac OS X 10_4_11; tr) AppleWebKit/528.4+ (KHTML, like Gecko) Version/4.0dp1 Safari/526.11.2");
   var userAgent = userAgentParser.parse();
 
   assertEquals("Safari", userAgent.getName());
@@ -227,8 +211,7 @@ UserAgentTest.prototype.testBrowserIsSafariWithPlusVersion = function() {
 
 UserAgentTest.prototype.testBrowserIsIEWithTridentVersion = function() {
   var userAgentParser = new webfont.UserAgentParser(
-      "Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; WOW64; " +
-      "Trident/4.0; SLCC2; .NET CLR 2.0.50727; InfoPath.2)");
+      "Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; WOW64; Trident/4.0; SLCC2; .NET CLR 2.0.50727; InfoPath.2)");
   var userAgent = userAgentParser.parse();
 
   assertEquals("MSIE", userAgent.getName());
@@ -243,8 +226,7 @@ UserAgentTest.prototype.testBrowserIsIEWithTridentVersion = function() {
 
 UserAgentTest.prototype.testBrowserGeckoShouldSupportWebFont = function() {
   var userAgentParser = new webfont.UserAgentParser(
-      "Mozilla/5.0 (Windows; U; Windows NT 5.1; ru-RU; rv:1.9.1.4) " +
-      "Gecko/20091016 (.NET CLR 3.5.30729)");
+      "Mozilla/5.0 (Windows; U; Windows NT 5.1; ru-RU; rv:1.9.1.4) Gecko/20091016 (.NET CLR 3.5.30729)");
   var userAgent = userAgentParser.parse();
 
   assertEquals("Mozilla", userAgent.getName());
@@ -257,8 +239,7 @@ UserAgentTest.prototype.testBrowserGeckoShouldSupportWebFont = function() {
 
 UserAgentTest.prototype.testBrowserGeckoNotExistingVersionShouldSupportWebFont = function() {
   var userAgentParser = new webfont.UserAgentParser(
-      "Mozilla/5.0 (Windows; U; Windows NT 5.1; ru-RU; rv:2.5.8) " +
-      "Gecko/20091016 (.NET CLR 3.5.30729)");
+      "Mozilla/5.0 (Windows; U; Windows NT 5.1; ru-RU; rv:2.5.8) Gecko/20091016 (.NET CLR 3.5.30729)");
   var userAgent = userAgentParser.parse();
 
   assertEquals("Mozilla", userAgent.getName());
@@ -271,8 +252,7 @@ UserAgentTest.prototype.testBrowserGeckoNotExistingVersionShouldSupportWebFont =
 
 UserAgentTest.prototype.testBrowserGeckoVer110ShouldSupportWebFont = function() {
   var userAgentParser = new webfont.UserAgentParser(
-      "Mozilla/5.0 (Windows; U; Windows NT 5.1; ru-RU; rv:1.10.1b) " +
-      "Gecko/20091016 (.NET CLR 3.5.30729)");
+      "Mozilla/5.0 (Windows; U; Windows NT 5.1; ru-RU; rv:1.10.1b) Gecko/20091016 (.NET CLR 3.5.30729)");
   var userAgent = userAgentParser.parse();
 
   assertEquals("Mozilla", userAgent.getName());
@@ -285,8 +265,7 @@ UserAgentTest.prototype.testBrowserGeckoVer110ShouldSupportWebFont = function() 
 
 UserAgentTest.prototype.testBrowserGeckoWeirdVerShouldNotSupportWebFont = function() {
   var userAgentParser = new webfont.UserAgentParser(
-      "Mozilla/5.0 (Windows; U; Windows NT 5.1; ru-RU; rv:1.b) " +
-      "Gecko/20091016 (.NET CLR 3.5.30729)");
+      "Mozilla/5.0 (Windows; U; Windows NT 5.1; ru-RU; rv:1.b) Gecko/20091016 (.NET CLR 3.5.30729)");
   var userAgent = userAgentParser.parse();
 
   assertEquals("Mozilla", userAgent.getName());
@@ -299,8 +278,7 @@ UserAgentTest.prototype.testBrowserGeckoWeirdVerShouldNotSupportWebFont = functi
 
 UserAgentTest.prototype.testBrowserGeckoYetAnotherWeirdVerShouldNotSupportWebFont = function() {
   var userAgentParser = new webfont.UserAgentParser(
-      "Mozilla/5.0 (Windows; U; Windows NT 5.1; ru-RU; rv:1.b) " +
-      "Gecko/20091016 (.NET CLR 3.5.30729)");
+      "Mozilla/5.0 (Windows; U; Windows NT 5.1; ru-RU; rv:1.b) Gecko/20091016 (.NET CLR 3.5.30729)");
   var userAgent = userAgentParser.parse();
 
   assertEquals("Mozilla", userAgent.getName());
@@ -313,8 +291,7 @@ UserAgentTest.prototype.testBrowserGeckoYetAnotherWeirdVerShouldNotSupportWebFon
 
 UserAgentTest.prototype.testBrowserGeckoNoSubVerShouldNotSupportWebFont = function() {
   var userAgentParser = new webfont.UserAgentParser(
-      "Mozilla/5.0 (Windows; U; Windows NT 5.1; ru-RU; rv:1.9) " +
-      "Gecko/20091016 (.NET CLR 3.5.30729)");
+      "Mozilla/5.0 (Windows; U; Windows NT 5.1; ru-RU; rv:1.9) Gecko/20091016 (.NET CLR 3.5.30729)");
   var userAgent = userAgentParser.parse();
 
   assertEquals("Mozilla", userAgent.getName());
@@ -327,8 +304,7 @@ UserAgentTest.prototype.testBrowserGeckoNoSubVerShouldNotSupportWebFont = functi
 
 UserAgentTest.prototype.testBrowserGeckoHighMinorVerShouldNotSupportWebFont = function() {
   var userAgentParser = new webfont.UserAgentParser(
-      "Mozilla/5.0 (Windows; U; Windows NT 5.1; ru-RU; rv:0.10.1) " +
-      "Gecko/20091016 (.NET CLR 3.5.30729)");
+      "Mozilla/5.0 (Windows; U; Windows NT 5.1; ru-RU; rv:0.10.1) Gecko/20091016 (.NET CLR 3.5.30729)");
   var userAgent = userAgentParser.parse();
 
   assertEquals("Mozilla", userAgent.getName());
@@ -341,8 +317,7 @@ UserAgentTest.prototype.testBrowserGeckoHighMinorVerShouldNotSupportWebFont = fu
 
 UserAgentTest.prototype.testBrowserGeckoHighSubVerShouldNotSupportWebFont = function() {
   var userAgentParser = new webfont.UserAgentParser(
-      "Mozilla/5.0 (Windows; U; Windows NT 5.1; ru-RU; rv:0.3.42) " +
-      "Gecko/20091016 (.NET CLR 3.5.30729)");
+      "Mozilla/5.0 (Windows; U; Windows NT 5.1; ru-RU; rv:0.3.42) Gecko/20091016 (.NET CLR 3.5.30729)");
   var userAgent = userAgentParser.parse();
 
   assertEquals("Mozilla", userAgent.getName());
