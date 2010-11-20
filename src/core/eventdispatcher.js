e7e361f64f7624625d425e229da4ddb9c3fe2ed3
Sean McBride <sean@typekit.com>
2010-11-20 13:19:15 -0800

    Changes to EventDispatcher so new tests for multiple loads pass.

--- src/core/eventdispatcher.js
+++ src/core/eventdispatcher.js
@@ -1,4 +1,9 @@
 /**
+ * A class to dispatch events and manage the event class names on an html
+ * element that represent the current state of fonts on the page. Active class
+ * names always overwrite inactive class names of the same type, while loading
+ * class names may be present whenever a font is loading (regardless of if an
+ * associated active or inactive class name is also present).
  * @param {webfont.DomHelper} domHelper
  * @param {HTMLElement} htmlElement
  * @param {Object} callbacks
@@ -44,6 +49,9 @@ webfont.EventDispatcher.INACTIVE = 'inactive';
  */
 webfont.EventDispatcher.FONT = 'font';
 
+/**
+ * Dispatch the loading event and append the loading class name.
+ */
 webfont.EventDispatcher.prototype.dispatchLoading = function() {
   this.domHelper_.appendClassName(this.htmlElement_,
       this.cssClassName_.build(
@@ -52,6 +60,7 @@ webfont.EventDispatcher.prototype.dispatchLoading = function() {
 };
 
 /**
+ * Dispatch the font loading event and append the font loading class name.
  * @param {string} fontFamily
  * @param {string} fontDescription
  */
@@ -64,6 +73,8 @@ webfont.EventDispatcher.prototype.dispatchFontLoading = function(fontFamily, fon
 };
 
 /**
+ * Dispatch the font active event, remove the font loading class name, remove
+ * the font inactive class name, and append the font active class name.
  * @param {string} fontFamily
  * @param {string} fontDescription
  */
@@ -71,6 +82,9 @@ webfont.EventDispatcher.prototype.dispatchFontActive = function(fontFamily, font
   this.domHelper_.removeClassName(this.htmlElement_,
       this.cssClassName_.build(
           this.namespace_, fontFamily, fontDescription, webfont.EventDispatcher.LOADING));
+  this.domHelper_.removeClassName(this.htmlElement_,
+      this.cssClassName_.build(
+          this.namespace_, fontFamily, fontDescription, webfont.EventDispatcher.INACTIVE));
   this.domHelper_.appendClassName(this.htmlElement_,
       this.cssClassName_.build(
           this.namespace_, fontFamily, fontDescription, webfont.EventDispatcher.ACTIVE));
@@ -79,6 +93,9 @@ webfont.EventDispatcher.prototype.dispatchFontActive = function(fontFamily, font
 };
 
 /**
+ * Dispatch the font inactive event, remove the font loading class name, and
+ * append the font inactive class name (unless the font active class name is
+ * already present).
  * @param {string} fontFamily
  * @param {string} fontDescription
  */
@@ -86,28 +103,48 @@ webfont.EventDispatcher.prototype.dispatchFontInactive = function(fontFamily, fo
   this.domHelper_.removeClassName(this.htmlElement_,
       this.cssClassName_.build(
           this.namespace_, fontFamily, fontDescription, webfont.EventDispatcher.LOADING));
-  this.domHelper_.appendClassName(this.htmlElement_,
+  var hasFontActive = this.domHelper_.hasClassName(this.htmlElement_,
       this.cssClassName_.build(
-          this.namespace_, fontFamily, fontDescription, webfont.EventDispatcher.INACTIVE));
+          this.namespace_, fontFamily, fontDescription, webfont.EventDispatcher.ACTIVE));
+  if (!hasFontActive) {
+    this.domHelper_.appendClassName(this.htmlElement_,
+        this.cssClassName_.build(
+            this.namespace_, fontFamily, fontDescription, webfont.EventDispatcher.INACTIVE));
+  }
   this.dispatch_(
       webfont.EventDispatcher.FONT + webfont.EventDispatcher.INACTIVE, fontFamily, fontDescription);
 };
 
+/**
+ * Dispatch the inactive event, remove the loading class name, and append the
+ * inactive class name (unless the active class name is already present).
+ */
 webfont.EventDispatcher.prototype.dispatchInactive = function() {
   this.domHelper_.removeClassName(this.htmlElement_,
       this.cssClassName_.build(
           this.namespace_, webfont.EventDispatcher.LOADING));
-  this.domHelper_.appendClassName(this.htmlElement_,
+  var hasActive = this.domHelper_.hasClassName(this.htmlElement_,
       this.cssClassName_.build(
-        this.namespace_, webfont.EventDispatcher.INACTIVE));
+          this.namespace_, webfont.EventDispatcher.ACTIVE));
+  if (!hasActive) {
+    this.domHelper_.appendClassName(this.htmlElement_,
+        this.cssClassName_.build(
+          this.namespace_, webfont.EventDispatcher.INACTIVE));
+  }
   this.dispatch_(webfont.EventDispatcher.INACTIVE);
 };
 
+/**
+ * Dispatch the active event, remove the loading class name, remove the inactive
+ * class name, and append the active class name.
+ */
 webfont.EventDispatcher.prototype.dispatchActive = function() {
-  // what about inactive? maybe if all fonts failed to load?
   this.domHelper_.removeClassName(this.htmlElement_,
       this.cssClassName_.build(
           this.namespace_, webfont.EventDispatcher.LOADING));
+  this.domHelper_.removeClassName(this.htmlElement_,
+      this.cssClassName_.build(
+          this.namespace_, webfont.EventDispatcher.INACTIVE));
   this.domHelper_.appendClassName(this.htmlElement_,
       this.cssClassName_.build(
           this.namespace_, webfont.EventDispatcher.ACTIVE));
