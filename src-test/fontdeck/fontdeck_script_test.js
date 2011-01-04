69ca47f7d0572a99707e60b9a289130119976149
Andy <andyhume@gmail.com>
2011-01-04 16:11:43 +0000

    Updated tests for live API.

--- src-test/fontdeck/fontdeck_script_test.js
+++ src-test/fontdeck/fontdeck_script_test.js
@@ -2,10 +2,10 @@ var FontdeckScriptTest = TestCase('FontdeckScriptTest');
 
 FontdeckScriptTest.prototype.testSupportAndLoadLifecycle = function() {
   var configuration = {
-    'id': '161'
+    'id': '2282'
   };
   var apiResponse = {
-    'css':'http://dev.int.fontdeck.com/s/css/uH5+KWQnibDTJRYggGJ9XZLTAgw/webfontloader/161.css',
+    'css':'http://fontdeck.com/s/css/uH5+KWQnibDTJRYggGJ9XZLTAgw/webfontloader/2282.css',
     'provides':[
       {'font_size_adjust':null,'weight':'normal','style':'normal','name':'Fertigo Pro Regular'},
       {'font_size_adjust':'0.5','weight':'normal','style':'normal','name':'Calluna Regular'}
@@ -34,14 +34,14 @@ FontdeckScriptTest.prototype.testSupportAndLoadLifecycle = function() {
 
   fontdeck.supportUserAgent(userAgent, function(support) { isSupport = support; });
   assertEquals('head', insert);
-  assertEquals('http://fontdeck.com/api/v1/project-info?project=161&domain=localhost&callback=window.__webfontfontdeckmodule__[161]', src);
+  assertEquals('http://fontdeck.com/api/v1/project-info?project=2282&domain=localhost&callback=window.__webfontfontdeckmodule__[2282]', src);
   assertEquals(null, isSupport);
 
   assertNotNull(global.__webfontfontdeckmodule__);
-  assertNotNull(global.__webfontfontdeckmodule__['161']);
+  assertNotNull(global.__webfontfontdeckmodule__['2282']);
   
   // Call the callback function passing in dummy API response.
-  global.__webfontfontdeckmodule__['161'](apiResponse);
+  global.__webfontfontdeckmodule__['2282'](apiResponse);
   
   assertEquals(fontdeck.fontFamilies_, [apiResponse.provides[0].name, apiResponse.provides[1].name]);
 
