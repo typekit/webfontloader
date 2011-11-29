3c63438523c107f1b63aa507c21d33ef701de017
Jeremie Lenfant-Engelmann <jeremiele@google.com>
2011-11-28 21:08:43 -0800

    Share code between strategies.

--- src/core/defaultcheckstrategy.js
+++ src/core/defaultcheckstrategy.js
@@ -3,32 +3,16 @@
  */
 webfont.DefaultCheckStrategy = function(sizingElementCreator, activeCallback,
     inactiveCallback, fontSizer, fontFamily, fontDescription, opt_fontTestString) {
-  this.sizingElementCreator_ = sizingElementCreator;
-  this.activeCallback_ = activeCallback;
-  this.inactiveCallback_ = inactiveCallback;
-  this.fontSizer_ = fontSizer;
-  this.fontFamily_ = fontFamily;
-  this.fontDescription_ = fontDescription;
-  this.fontTestString_ = opt_fontTestString
-      || webfont.FontWatchRunner.DEFAULT_TEST_STRING;
+  webfont.DefaultCheckStrategy.superCtor_.call(this, sizingElementCreator,
+      activeCallback, inactiveCallback, fontSizer, fontFamily, fontDescription,
+      opt_fontTestString);
 };
+webfont.extendsClass(webfont.BaseCheckStrategy, webfont.DefaultCheckStrategy);
 
 webfont.DefaultCheckStrategy.prototype.setUp = function() {
-  this.originalSizeA_ = this.getStackSize_(webfont.FontWatchRunner.SANS_STACK);
-  this.originalSizeB_ = this.getStackSize_(webfont.FontWatchRunner.SERIF_STACK);
+  webfont.DefaultCheckStrategy.super_.setUp.call(this);
   this.lastObservedSizeA_ = this.originalSizeA_;
   this.lastObservedSizeB_ = this.originalSizeB_;
-  this.requestedFontA_ = this.sizingElementCreator_.createSizingElement(
-      this.fontFamily_, webfont.FontWatchRunner.SANS_STACK,
-      this.fontDescription_, this.fontTestString_);
-  this.requestedFontB_ = this.sizingElementCreator_.createSizingElement(
-      this.fontFamily_, webfont.FontWatchRunner.SERIF_STACK,
-      this.fontDescription_, this.fontTestString_);
-};
-
-webfont.DefaultCheckStrategy.prototype.tearDown = function() {
-  this.sizingElementCreator_.deleteSizingElement(this.requestedFontA_);
-  this.sizingElementCreator_.deleteSizingElement(this.requestedFontB_);
 };
 
 webfont.DefaultCheckStrategy.prototype.isLoaded = function() {
@@ -54,12 +38,4 @@ webfont.DefaultCheckStrategy.prototype.getTimeoutCallback = function() {
   return webfont.bind(this, function() {
       this.inactiveCallback_(this.fontFamily_, this.fontDescription_);
   });
-};
-
-webfont.DefaultCheckStrategy.prototype.getStackSize_ = function(stack) {
-  var stackElement = this.sizingElementCreator_.createSizingElement('',
-      stack, this.fontDescription_, this.fontTestString_);
-  var size = this.fontSizer_.getWidth(stackElement);
-  this.sizingElementCreator_.deleteSizingElement(stackElement);
-  return size;
 };
--- src/core/lastresortwebkitcheckstrategy.js
+++ src/core/lastresortwebkitcheckstrategy.js
@@ -3,31 +3,16 @@
  */
 webfont.LastResortWebKitCheckStrategy = function(sizingElementCreator, activeCallback,
     inactiveCallback, fontSizer, fontFamily, fontDescription, opt_fontTestString) {
-  this.sizingElementCreator_ = sizingElementCreator;
-  this.activeCallback_ = activeCallback;
-  this.inactiveCallback_ = inactiveCallback;
-  this.fontSizer_ = fontSizer;
-  this.fontFamily_ = fontFamily;
-  this.fontDescription_ = fontDescription;
-  this.fontTestString_ = opt_fontTestString
-      || webfont.FontWatchRunner.DEFAULT_TEST_STRING;
+  webfont.LastResortWebKitCheckStrategy.superCtor_.call(this,
+      sizingElementCreator, activeCallback, inactiveCallback, fontSizer,
+      fontFamily, fontDescription, opt_fontTestString);
 };
+webfont.extendsClass(webfont.BaseCheckStrategy,
+    webfont.LastResortWebKitCheckStrategy);
 
 webfont.LastResortWebKitCheckStrategy.prototype.setUp = function() {
-  this.originalSizeA_ = this.getStackSize_(webfont.FontWatchRunner.SANS_STACK);
-  this.originalSizeB_ = this.getStackSize_(webfont.FontWatchRunner.SERIF_STACK);
+  webfont.LastResortWebKitCheckStrategy.super_.setUp.call(this);
   this.webKitLastResortFontSizes_ = this.setUpWebKitLastResortFontSizes_();
-  this.requestedFontA_ = this.sizingElementCreator_.createSizingElement(
-      this.fontFamily_, webfont.FontWatchRunner.SANS_STACK,
-      this.fontDescription_, this.fontTestString_);
-  this.requestedFontB_ = this.sizingElementCreator_.createSizingElement(
-      this.fontFamily_, webfont.FontWatchRunner.SERIF_STACK,
-      this.fontDescription_, this.fontTestString_);
-};
-
-webfont.LastResortWebKitCheckStrategy.prototype.tearDown = function() {
-  this.sizingElementCreator_.deleteSizingElement(this.requestedFontA_);
-  this.sizingElementCreator_.deleteSizingElement(this.requestedFontB_);
 };
 
 webfont.LastResortWebKitCheckStrategy.prototype.isLoaded = function() {
@@ -46,14 +31,6 @@ webfont.LastResortWebKitCheckStrategy.prototype.getActiveCallback = function() {
 
 webfont.LastResortWebKitCheckStrategy.prototype.getTimeoutCallback = function() {
   return this.getActiveCallback();
-};
-
-webfont.LastResortWebKitCheckStrategy.prototype.getStackSize_ = function(stack) {
-  var stackElement = this.sizingElementCreator_.createSizingElement('',
-      stack, this.fontDescription_, this.fontTestString_);
-  var size = this.fontSizer_.getWidth(stackElement);
-  this.sizingElementCreator_.deleteSizingElement(stackElement);
-  return size;
 };
 
 /**
