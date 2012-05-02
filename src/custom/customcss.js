fa3f12979a8f0d68b46c9d2ef6c0e31ea6180342
Jesse Grosjean <jesse@hogbaysoftware.com>
2012-05-02 17:05:56 -0400

    Add google style font variations to custom module.

--- src/custom/customcss.js
+++ src/custom/customcss.js
@@ -11,6 +11,7 @@
 webfont.CustomCss = function(domHelper, configuration) {
   this.domHelper_ = domHelper;
   this.configuration_ = configuration;
+  this.fvd_ = new webfont.FontVariationDescription();
 };
 
 webfont.CustomCss.NAME = 'custom';
@@ -21,10 +22,12 @@ webfont.CustomCss.prototype.load = function(onReady) {
 
   for (var i = 0, len = urls.length; i < len; i++) {
     var url = urls[i];
-
     this.domHelper_.insertInto('head', this.domHelper_.createCssLink(url));
   }
-  onReady(families);
+
+  var fontApiParser = new webfont.FontApiParser(families);
+  fontApiParser.parse();
+  onReady(fontApiParser.getFontFamilies(), fontApiParser.getVariations(), fontApiParser.getFontTestStrings());
 };
 
 webfont.CustomCss.prototype.supportUserAgent = function(userAgent, support) {
