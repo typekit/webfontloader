40c7eade64ffd2e8b600b427646e154967334d12
Bram Stein <b.l.stein@gmail.com>
2013-01-08 14:43:43 +0100

    Added Apple Color Emoji to the generic (last resort) font families. According to http://www.opensource.apple.com/source/WebCore/WebCore-955.66/platform/graphics/mac/FontCacheMac.mm?txt, Apple Color Emoji is used as a fall back font when it is available. Since it is available on all iOS versions we'll always get it before any other fallback font. Since it doesn't match any of the other generic fonts either we'll have to add it to our list.

--- src/core/fontwatchrunner.js
+++ src/core/fontwatchrunner.js
@@ -41,8 +41,7 @@ webfont.FontWatchRunner.GenericFontFamily = {
   SERIF: 'serif',
   SANS_SERIF: 'sans-serif',
   MONOSPACE: 'monospace',
-  CURSIVE: 'cursive',
-  FANTASY: 'fantasy'
+  EMOJI: 'Apple Color Emoji'
 };
 
 /**
