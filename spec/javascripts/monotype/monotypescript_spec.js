e2b09ab687b6cfd406f9cdf257d6595a21290b18
Bram Stein <b.l.stein@gmail.com>
2013-01-25 13:14:31 +0100

    Converted MonotypeScript.

---
+++ spec/javascripts/monotype/monotypescript_spec.js
@@ -0,0 +1,48 @@
+describe('MonotypeScript', function () {
+  var MonotypeScript = webfont.MonotypeScript,
+      UserAgent = webfont.UserAgent;
+
+  var configuration = {
+    projectId: '01e2ff27-25bf-4801-a23e-73d328e6c7cc',
+    api: 'http://fast.fonts.com/jsapidev'
+  };
+
+  var fakeDomHelper = null,
+      global = null,
+      script = {},
+      monotype = null,
+      load =  null,
+      useragent = null,
+      support = null;
+
+  beforeEach(function () {
+    global = {};
+
+    fakeDomHelper = {
+      createElement: jasmine.createSpy('createElement').andReturn(script),
+      insertInto: jasmine.createSpy('insertInto'),
+      getLoadWindow: jasmine.createSpy('getLoadWindow').andReturn(global),
+      getProtocol: jasmine.createSpy('getProtocol').andReturn('http:')
+    };
+    support = jasmine.createSpy('support');
+    load = jasmine.createSpy('load');
+    useragent = new UserAgent('Test', '1.0', true);
+
+    monotype = new MonotypeScript(useragent, fakeDomHelper, configuration);
+    monotype.supportUserAgent(useragent, support);
+    monotype.load(load);
+
+    global[MonotypeScript.HOOK + configuration.projectId] = function () {
+      return ['aachen bold', 'kid print regular'];
+    };
+
+    script.onload();
+  });
+
+  it('should create a script element', function () {
+    expect(support).toHaveBeenCalled();
+    expect(fakeDomHelper.createElement).toHaveBeenCalledWith('script');
+    expect(script.src).toEqual('http://fast.fonts.com/jsapidev/01e2ff27-25bf-4801-a23e-73d328e6c7cc.js');
+    expect(load).toHaveBeenCalledWith([undefined, undefined], {});
+  });
+});
