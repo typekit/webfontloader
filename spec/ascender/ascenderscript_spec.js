d7c4b83c1ce9de57293c966b87f82bf6c48f0835
Bram Stein <stein@adobe.com>
2013-04-04 11:25:34 +0200

    Add more test to the Ascender module.

--- spec/ascender/ascenderscript_spec.js
+++ spec/ascender/ascenderscript_spec.js
@@ -1,5 +1,6 @@
 describe('AscenderScript', function () {
-  var AscenderScript = webfont.AscenderScript;
+  var AscenderScript = webfont.AscenderScript,
+      Font = webfont.Font;
 
   var configuration = {
     key: 'ec2de397-11ae-4c10-937f-bf94283a70c1',
@@ -29,6 +30,14 @@ describe('AscenderScript', function () {
       expect(fakeDomHelper.insertInto.calls[0].args[0]).toEqual('head');
     });
 
+    it('should pass the fonts correctly', function () {
+      expect(fakeOnReady).toHaveBeenCalledWith([
+        new Font('AndyBold', 'n4'),
+        new Font('Arial', 'n7'),
+        new Font('Arial', 'n4')
+      ]);
+    });
+
     it('should parse variations correctly', function () {
       expect(ascenderScript.parseVariations('regular')).toEqual(['n4']);
       expect(ascenderScript.parseVariations('bold')).toEqual(['n7']);
@@ -38,6 +47,20 @@ describe('AscenderScript', function () {
       expect(ascenderScript.parseVariations('regular,bold')).toEqual(['n4', 'n7']);
       expect(ascenderScript.parseVariations('regular,,bold')).toEqual(['n4', 'n7']);
       expect(ascenderScript.parseVariations('n4,n7')).toEqual(['n4', 'n7']);
+    });
+
+    it('should parse font families correctly', function () {
+      expect(ascenderScript.parseFamilyAndVariations('Arial')).toEqual([new Font('Arial')]);
+      expect(ascenderScript.parseFamilyAndVariations('Arial:bold,regular')).toEqual([new Font('Arial', 'n7'), new Font('Arial', 'n4')]);
+      expect(ascenderScript.parseFamilyAndVariations('Arial:n4,n7')).toEqual([new Font('Arial', 'n4'), new Font('Arial', 'n7')]);
+    });
+
+    it('should parse multiple font families correctly', function () {
+      expect(ascenderScript.parseFamiliesAndVariations(['Arial', 'Sans:n4,n7'])).toEqual([
+        new Font('Arial', 'n4'),
+        new Font('Sans', 'n4'),
+        new Font('Sans', 'n7')
+      ]);
     });
   })
 });
