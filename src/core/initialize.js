49d84cbc9f3b10695a1a75e21203436920bfd19f
Bram Stein <stein@adobe.com>
2013-05-21 12:29:20 +0200

    Change modules to be included individually. Custom module is included by default.

--- src/core/initialize.js
+++ src/core/initialize.js
@@ -17,7 +17,32 @@ webfont.FontTestStrings;
 /**
  * @define {boolean}
  */
-var INCLUDE_MODULES = true;
+var INCLUDE_ASCENDER_MODULE = false;
+
+/**
+ * @define {boolean}
+ */
+var INCLUDE_CUSTOM_MODULE = true;
+
+/**
+ * @define {boolean}
+ */
+var INCLUDE_FONTDECK_MODULE = false;
+
+/**
+ * @define {boolean}
+ */
+var INCLUDE_MONOTYPE_MODULE = false;
+
+/**
+ * @define {boolean}
+ */
+var INCLUDE_TYPEKIT_MODULE = false;
+
+/**
+ * @define {boolean}
+ */
+var INCLUDE_GOOGLE_MODULE = false;
 
 /**
  * @define {string}
@@ -34,27 +59,37 @@ var WEBFONT_CONFIG = 'WebFontConfig';
  */
 var webFontLoader = new webfont.WebFont(goog.global);
 
-if (INCLUDE_MODULES) {
+if (INCLUDE_ASCENDER_MODULE) {
   webFontLoader.addModule('ascender', function (configuration, domHelper) {
     return new webfont.AscenderScript(domHelper, configuration);
   });
+}
 
+if (INCLUDE_CUSTOM_MODULE) {
   webFontLoader.addModule('custom', function (configuration, domHelper) {
     return new webfont.CustomCss(domHelper, configuration);
   });
+}
 
+if (INCLUDE_FONTDECK_MODULE) {
   webFontLoader.addModule('fontdeck', function (configuration, domHelper) {
     return new webfont.FontdeckScript(domHelper, configuration);
   });
+}
 
+if (INCLUDE_MONOTYPE_MODULE) {
   webFontLoader.addModule('monotype', function (configuration, domHelper) {
     return new webfont.MonotypeScript(domHelper, configuration);
   });
+}
 
+if (INCLUDE_TYPEKIT_MODULE) {
   webFontLoader.addModule('typekit', function (configuration, domHelper) {
     return new webfont.TypekitScript(domHelper, configuration);
   });
+}
 
+if (INCLUDE_GOOGLE_MODULE) {
   webFontLoader.addModule('google', function (configuration, domHelper) {
     return new webfont.GoogleFontApi(domHelper, configuration);
   });
