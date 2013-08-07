ea89b7abbfecc9950be45ca42eccc04e21828570
Bram Stein <stein@adobe.com>
2013-08-07 15:17:13 +0200

    Update jasmine-browserstack and reduce the number of browsers we automatically test on.

--- tools/jasmine-browserstack/jasmine-browserstack.js
+++ tools/jasmine-browserstack/jasmine-browserstack.js
@@ -1,12 +1,27 @@
 (function (root, factory) {
-  if (typeof exports === 'object') {
-    module.exports = factory(require('jasmine'));
-  } else if (typeof define === 'function' && define.amd) {
+  if (typeof define === 'function' && define.amd) {
     define(['jasmine'], factory);
   } else {
     factory(jasmine);
   }
 }(this, function (jasmine) {
+  function stack(err) {
+    var str = err.stack || err.toString();
+
+    if (!~str.indexOf(err.message)) {
+      str = err.message + '\n' + str;
+    }
+
+    if ('[object Error]' == str) {
+      str = err.message;
+    }
+
+    if (!err.stack && err.sourceURL && err.line !== undefined) {
+      str += '\n(' + err.sourceURL + ':' + err.line + ')';
+    }
+    return str.replace(/^/gm, '  ');
+  }
+
   function BrowserStackReporter() {
     this.stats = {
       suites: 0,
@@ -16,8 +31,6 @@
       failures: 0
     };
     this.tests = [];
-    this.failures = [];
-    this.passes = [];
   }
 
   BrowserStackReporter.prototype.reportRunnerStarting = function (runner) {
@@ -32,48 +45,35 @@
     var currentTime = new Date().getTime();
     spec.duration = currentTime - spec.startedAt;
 
+    var result = spec.results();
+
     var test = {
-      title: spec.description,
-      fullTitle: spec.getFullName(),
+      status: null,
+      title: spec.getFullName().replace(/#/g, ''),
       duration: currentTime - spec.startedAt
     };
 
-    this.tests.push(test);
-
-    var result = spec.results();
-
     if (result.skipped) {
       this.stats.pending += 1;
+      test.status = 'skipped';
     } else if (result.failedCount === 0) {
-      this.passes.push(test);
+      this.stats.passes += 1;
+      test.status = 'passed';
     } else {
       var items = result.getItems(),
-          message = '';
+          message = [];
 
       for (var i = 0; i < items.length; i += 1) {
-        var str = items[i].trace.stack || items[i].trace.toString();
-
-        if (!~str.indexOf(items[i].trace.message)) {
-          str = items[i].trace.message + '\n' + str;
-        }
-
-        if ('[object Error]' == str) {
-          str = items[i].trace.message;
-        }
-
-        if (!items[i].trace.stack && items[i].trace.sourceURL && items[i].trace.line !== undefined) {
-          str += '\n(' + items[i].trace.sourceURL + ':' + items[i].trace.line + ')';
-        }
-
-        message += str;
+        message.push(stack(items[i].trace));
       }
 
-      test.err = {
-        message: message
-      };
-
-      this.failures.push(test);
+      test.err = message;
+      test.status = 'failed';
+      this.stats.failures += 1;
     }
+
+    this.stats.tests += 1;
+    this.tests.push(test);
   };
 
   BrowserStackReporter.prototype.reportSuiteResults = function (suite) {
@@ -87,26 +87,39 @@
 
     this.stats.suites = suites.length;
 
-    for (var i = 0; i < suites.length; i += 1) {
-      if (suites[i].parentSuite === null) {
-        this.stats.tests += suites[i].specs().length;
+    var result = '1..' + this.stats.tests + '\n';
+
+    for (var i = 0; i < this.tests.length; i += 1) {
+      var count = i + 1;
+
+      if (this.tests[i].status === 'pending') {
+        result += 'ok ' + count + ' ' + this.tests[i].title + ' # SKIP -\n';
+      } else if (this.tests[i].status === 'failed') {
+        result += 'not ok ' + count + ' ' + this.tests[i].title + '\n';
+        for (var j = 0; j < this.tests[i].err.length; j += 1) {
+          result += this.tests[i].err[j] + '\n';
+        }
+      } else {
+        result += 'ok ' + count + ' ' + this.tests[i].title + '\n';
       }
     }
 
-    this.stats.failures = this.failures.length;
-    this.stats.passes = this.passes.length;
+    result += '# tests ' + this.stats.tests + '\n';
+    result += '# pass ' + this.stats.passes + '\n';
+    result += '# fail ' + this.stats.failures + '\n';
 
-    if (/browserstack=true/i.test(window.location.search)) {
-      var xhr = new XMLHttpRequest();
+    if (/browser=/i.test(window.location.search)) {
+      var xhr = null;
+
+      if (window.XMLHttpRequest) {
+        xhr = new XMLHttpRequest();
+      } else {
+        xhr = new ActiveXObject('Microsoft.XMLHTTP');
+      }
 
       xhr.open('POST', window.location.href);
-      xhr.setRequestHeader('Content-Type', 'application/json');
-      xhr.send(JSON.stringify({
-        stats: this.stats,
-        tests: this.tests,
-        failures: this.failures,
-        passes: this.passes
-      }, null, 2));
+      xhr.setRequestHeader('Content-Type', 'text/plain');
+      xhr.send(result);
     }
   };
 
