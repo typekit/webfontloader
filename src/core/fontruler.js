c00314cdd30256c1eaad19b619b26bbba6c168c1
Bram Stein <stein@adobe.com>
2014-03-01 09:36:34 +0100

    Add display:block to FontRuler so it can not be easily overridden.

--- src/core/fontruler.js
+++ src/core/fontruler.js
@@ -38,7 +38,7 @@ goog.scope(function () {
    * @return {string}
    */
   FontRuler.prototype.computeStyleString_ = function(font) {
-    return "position:absolute;top:-999px;left:-999px;" +
+    return "display:block;position:absolute;top:-999px;left:-999px;" +
            "font-size:300px;width:auto;height:auto;line-height:normal;margin:0;" +
            "padding:0;font-variant:normal;white-space:nowrap;font-family:" +
            font.getCssName() + ";" + font.getCssVariation();
