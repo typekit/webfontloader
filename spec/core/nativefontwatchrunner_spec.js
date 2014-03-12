554106acc26e2aaef10a8c5db39463091343179f
Bram Stein <stein@adobe.com>
2014-03-12 13:50:24 +0100

    Clean up native font watch runner specs.

--- spec/core/nativefontwatchrunner_spec.js
+++ spec/core/nativefontwatchrunner_spec.js
@@ -49,11 +49,11 @@ describe('NativeFontWatchRunner', function () {
     });
 
     it('should load font succesfully', function () {
-      var fontWatchRunner = new FontWatchRunner(activeCallback, inactiveCallback,
-          domHelper, sourceSansC, userAgent.getBrowserInfo(), 5000),
+      var fontWatchRunner = new NativeFontWatchRunner(activeCallback, inactiveCallback,
+          domHelper, sourceSansC),
           ruler = new FontRuler(domHelper, 'abcdef'),
           monospace = new Font('monospace'),
-          sourceSansCFallback = new Font("'SourceSansC', monospace"),
+          sourceSansCFallback = new Font('SourceSansC, monospace'),
           activeWidth = null,
           originalWidth = null,
           finalCheck = false;
@@ -91,8 +91,8 @@ describe('NativeFontWatchRunner', function () {
     });
 
     it('should attempt to load a non-existing font', function () {
-      var fontWatchRunner = new FontWatchRunner(activeCallback, inactiveCallback,
-          domHelper, elena, userAgent.getBrowserInfo(), 500);
+      var fontWatchRunner = new NativeFontWatchRunner(activeCallback, inactiveCallback,
+          domHelper, elena);
 
       runs(function () {
         fontWatchRunner.start();
@@ -104,54 +104,6 @@ describe('NativeFontWatchRunner', function () {
 
       runs(function () {
         expect(inactiveCallback).toHaveBeenCalledWith(elena);
-      });
-    });
-
-    it('should load even if @font-face is inserted after watching has started', function () {
-      var fontWatchRunner = new FontWatchRunner(activeCallback, inactiveCallback,
-          domHelper, sourceSansD, userAgent.getBrowserInfo(), 5000),
-          ruler = new FontRuler(domHelper, 'abcdef'),
-          monospace = new Font('monospace'),
-          sourceSansDFallback = new Font("'SourceSansD', monospace"),
-          activeWidth = null,
-          originalWidth = null,
-          finalCheck = false;
-
-      runs(function () {
-        ruler.insert();
-        ruler.setFont(monospace);
-        originalWidth = ruler.getWidth();
-        ruler.setFont(sourceSansDFallback);
-        fontWatchRunner.start();
-        var link = document.createElement('link');
-
-        link.rel = "stylesheet";
-        link.href= "fixtures/fonts/sourcesansd.css";
-
-        domHelper.insertInto('head', link);
-      });
-
-      waitsFor(function () {
-        return activeCallback.wasCalled || inactiveCallback.wasCalled;
-      });
-
-      runs(function () {
-        expect(activeCallback).toHaveBeenCalledWith(sourceSansD);
-        activeWidth = ruler.getWidth();
-        expect(activeWidth).not.toEqual(originalWidth);
-
-        window.setTimeout(function () {
-          finalCheck = true;
-        }, 200);
-      });
-
-      waitsFor(function () {
-        return finalCheck;
-      });
-
-      runs(function () {
-        expect(ruler.getWidth()).not.toEqual(originalWidth);
-        expect(ruler.getWidth()).toEqual(activeWidth);
       });
     });
   }
