18b751c53446d508a0de46d525107d98751ee32a
Bram Stein <stein@adobe.com>
2014-05-02 11:37:06 +0200

    Release 1.5.4

--- lib/webfontloader.rb
+++ lib/webfontloader.rb
@@ -3,7 +3,7 @@ require 'yaml'
 require 'webfontloader/modules'
 
 module WebFontLoader
-  VERSION = '1.5.3'
+  VERSION = '1.5.4'
 
   ProjectRoot = File.expand_path(File.dirname(__FILE__) + "/..")
 
